8195c210b84b1a21d50d469bb59171a971899fc8
Sam Owens <git@sqeez.co.uk>
2024-06-26 20:29:34 +0100

    README update and example code removal

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -1,17 +1,7 @@
-import { useState } from "react";
-
 const Home = () => {
-  const [name, setName] = useState("Sam");
-
-  const handleClick = () => {
-    setName("Alan");
-  };
-
   return (
     <div className="home p-4">
       <h2 className="text-2xl font-medium">Homepage</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
