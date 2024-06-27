cae7775b3860c34332013f39f18813d2a07dc0d0
Sam Owens <git@sqeez.co.uk>
2024-06-27 15:55:40 +0100

    Tutorial #8
    https://www.youtube.com/watch?v=4pO-HcG2igk

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+  let testData = "Value Z";
+
+  const [name, setName] = useState("Sam");
+  const [testValue, setTestValue] = useState("Value X");
+
+  const handleClick = () => {
+    setName("Shola");
+    setTestValue(testData);
+  };
+
   return (
     <div className="home p-4">
       <h2 className="text-2xl font-medium">Homepage</h2>
+      <p>{name}</p>
+      <p>{testValue}</p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
