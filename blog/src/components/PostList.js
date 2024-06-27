fc641fe3e1961c4ea8371f1285767b7662542693
Sam Owens <git@sqeez.co.uk>
2024-06-27 23:57:25 +0100

    Tutorial #13 - Functions as Props

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,15 +1,23 @@
+import { TbCircleXFilled } from 'react-icons/tb';
+
 const PostList = ({ posts, title }) => {
   return (
     <div className="post-list max-w-3xl m-auto p4">
       <h2 className="text-2xl font-semibold mb-4">{title}</h2>
       {posts.map((post) => (
         <div
-          className="post-card p-4 mb-4 rounded-md bg-slate-200 shadow-md hover:bg-slate-300 cursor-pointer"
+          className="group p-4 mb-4 rounded-md bg-slate-200 shadow-md hover:bg-slate-300 cursor-pointer relative transition"
           key={post.id}
         >
           <h2 className="font-semibold text-xl mb-2">{post.title}</h2>
           <p className="pb-4">{post.intro}</p>
           <p className="text-xs text-right">Written by {post.author}</p>
+          <button
+            className="absolute top right -top-3 -right-3"
+            onClick={() => {}}
+          >
+            <TbCircleXFilled className="red h-8 w-8 text-slate-800 hover:text-slate-900 invisible group-hover:visible transition" />
+          </button>
         </div>
       ))}
     </div>
