32e2ecadc3d84728cfff6eb7254c478575efd716
Sam Owens <git@sqeez.co.uk>
2024-06-29 22:52:28 +0100

    Tutorial #18 - Conditional Loading Message
    https://www.youtube.com/watch?v=qtheqr0jgIQ

--- blog/src/components/Home.js
+++ blog/src/components/Home.js
@@ -3,25 +3,21 @@ import PostList from './PostList';
 
 const Home = () => {
   const [posts, setPosts] = useState(null);
-  // Initializing the 'posts' state variable to null using the useState hook
-  // 'setPosts' is the function to update the 'posts' state
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Defining a useEffect hook that runs once after the component mounts
-
     fetch('http://localhost:8000/posts')
-      // Making a GET request to fetch posts from the server
       .then((res) => res.json())
-      // Parsing the response as JSON
-      .then((data) => setPosts(data));
-    // Updating the 'posts' state with the fetched data
+      .then((data) => {
+        setPosts(data);
+        setIsLoading(false);
+      });
   }, []);
-  // The empty dependency array means this useEffect runs only once, when the component mounts
 
   return (
     <div className="home p-4">
+      {isLoading && <div>Loading...</div>}
       {posts && (
-        // Conditional rendering: only render PostList if 'posts' is not null
         <PostList
           posts={posts}
           title="All Posts"
