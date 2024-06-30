c6971fc401609bdf3095ed80285666953ebb19ea
Sam Owens <git@sqeez.co.uk>
2024-06-30 16:23:15 +0100

    Tutorial #31 - Delete Request
    https://www.youtube.com/watch?v=Wb-0CkLiyQk

--- blog/src/components/Post.js
+++ blog/src/components/Post.js
@@ -1,14 +1,25 @@
 import { useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
+import { useHistory } from 'react-router-dom';
 
 const PostDetails = () => {
   const { id } = useParams();
+
+  const history = useHistory();
 
   const {
     data: post,
     isLoading,
     error,
   } = useFetch(`http://localhost:8000/posts/${id}`);
+
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/posts/${post.id}`, {
+      method: 'DELETE',
+    }).then(() => {
+      history.push('/');
+    });
+  };
 
   return (
     <div className="post content-container">
@@ -23,11 +34,17 @@ const PostDetails = () => {
         </div>
       )}
       {post && (
-        <div className="post-wrapper">
+        <article className="flex flex-col">
           <h2 className="text-2xl font-semibold mb-4">{post.title}</h2>
           <p className="pb-4">{post.content}</p>
-          <p className="text-xs text-right">Written by {post.author}</p>
-        </div>
+          <p className="text-xs text-right mb-4">Written by {post.author}</p>
+          <button
+            onClick={handleDelete}
+            className="button font-semibold text-black bg-red-500 hover:bg-red-600 m-auto "
+          >
+            Delete Post
+          </button>
+        </article>
       )}
     </div>
   );
