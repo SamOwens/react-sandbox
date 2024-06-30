98dd21f541bbbf4638db027c6c7ea2264bee5fcc
Sam Owens <git@sqeez.co.uk>
2024-06-30 13:51:25 +0100

    Tutorial #23 - Router Links
    https://www.youtube.com/watch?v=DO-pSysGItQ

--- blog/src/components/Header.js
+++ blog/src/components/Header.js
@@ -1,22 +1,24 @@
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
     <header className="w-full p-4 flex justify-between bg-slate-800 items-center">
       <h1 className="text-white text-2xl font-medium">React Blog</h1>
       <div className="links flex justify-center">
-        <a
-          href="/"
+        <Link
+          to="/"
           className="me-4 p-2 text-white bg-slate-600 rounded-md hover:bg-slate-700 transition-all duration-200 ease-in-out"
           aria-label="Return to the homepage"
         >
           Home
-        </a>
-        <a
-          href="/create"
+        </Link>
+        <Link
+          to="/create"
           className="p-2 text-white bg-slate-600 rounded-md hover:bg-slate-700 transition-all duration-200 ease-in-out"
           aria-label="Create new blog post"
         >
           New Post
-        </a>
+        </Link>
       </div>
     </header>
   );
